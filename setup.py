f26c5b7ae45a9f7f04a4b6ab29779ceaf0ee1c45
Tara Parker <tara.parker@sjsu.edu>
2023-12-03 19:57:06 -0800

    setup.py and init files

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='changed-toolbox',
+      version='1.0',
+      description='Modules for CHANGED',
+      author='Tara Parker',
+      author_email='tara.parker@sjsu.edu',
+      maintainer='Mike Wood',
+      maintainer_email='mike.wood@sjsu.edu',
+      url='https://github.com/tparker1/CHANGED/tree/main',
+      packages=['interpolation', 'toolbox', 'toolbox.initiation'])
